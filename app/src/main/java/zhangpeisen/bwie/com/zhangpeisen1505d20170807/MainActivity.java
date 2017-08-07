480d4309087b3b6f032566ba81578adffde2f54d
zxc897879012 <897879012@qq.com>
2017-08-07 08:58:22 +0800

    Initial commit第一次提交

---
+++ app/src/main/java/zhangpeisen/bwie/com/zhangpeisen1505d20170807/MainActivity.java
@@ -0,0 +1,16 @@
+package zhangpeisen.bwie.com.zhangpeisen1505d20170807;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        
+    }
+}
